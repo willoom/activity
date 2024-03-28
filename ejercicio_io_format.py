4e1c3c728f52a911f944b798f483321c525e82f4
willoom <137672356+willoom@users.noreply.github.com>
2024-03-28 19:50:23 +0100

    Add files via upload

---
+++ ejercicio_io_format.py
@@ -0,0 +1,29 @@
+"""
+Pide al usuario que introduzca un número entero que almacenarás 
+en la variable número.
+
+Visualiza el mensaje: 
+
+    'Introduzca el número a tranformar en binario, octal y hexadecimal: '
+
+Cuando asignes el valor, a la variable número, asegúrate que lo haces como entero. 
+
+"""
+
+# ...
+ 
+"""
+Escribe por pantalla, protegiendo un espacio de diez caracteres, alineados a la derecha
+y separados por espacios la transformación del número de forma que 
+para una entrada hipotética del número 1 la salida será:
+
+'         0b1' en binario, '        0o1' en octal y '        0x1' en hexadecimal es el número 1 en decimal.
+
+Otro ejemplo:
+Introduzca el número a tranformar en binario, octal y hexadecimal: 111
+' 0b1101111' en binario, '     0o157' en octal y '      0x6f' en hexadecimal es el número 111 en decimal.
+
+"""
+
+# ...
+
