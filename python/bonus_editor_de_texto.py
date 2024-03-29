5e9e3edc41bebb35ea9377dea64c6098579f66c6
willoom <137672356+willoom@users.noreply.github.com>
2024-03-29 10:24:47 +0100

    Create bonus_editor_de_texto.py

---
+++ python/bonus_editor_de_texto.py
@@ -0,0 +1,46 @@
+"""
+CARÁCTER.
+Esta actividad es INDIVIDUAL.
+
+ÁMBITO.
+Os bonificará hasta +5 puntos la actividad de editor de texto, 
+no pudiendo superar la nota final de la actividad los 10 puntos.
+Estará disponible hasta el 14 de abril.
+
+PRE-REQUISITO.
+Para optar a este bonificador, tu grupo debe haber entregado 
+el proyecto Buscaminas.
+
+RÚBRICA.
++2,5 Subsanar TODOS los errores de la actividad. No quiero ver 
+un código diferente. Quiero ver vuestro código modificado.
+
++0,25 Incluir una nueva opción e) que sea:  
+"e) Cifrar con clave y operación XOR." y una opción 
+"f) Descifrar con clave y operación XOR."
+
++0,25 Solicitar al usuario una clave, tanto si se pulsa la opción e) 
+como la f).
+
++1 La opción e) cambiará lo que aparece a continuación de Escrito: 
+con el texto codificado.
+
++1 La opción f) cambiará lo que aparece a continuación de Escrito: 
+con el texto descodificado.
+
+CIFRADO Y DESCIFRADO UTILIZADO.
+
+La clave la usaremos de forma circular tantas veces como sea necesaria.
+
+Ejemplo:
+Con Escrito: En un agujero vivía un hobbit codifico con la clave "casa".
+Tanto codificación como descodificación se realizaría aplicando casa a
+En u|n ag|ujer|o vi|vía |un h|obbi|t.
+casa casa casa casa casa casa casa ca
+Fíjate que las dos últimas letras no llegan a terminar la clave.
+No debes usar 'sa' de 'casa'.
+
+Ten en cuenta que para realizar la operación XOR has de trabajar 
+con los códigos de los caracteres, tanto de las líneas de Escrito:, 
+como de la clave.
+"""
