17e0fc6fc1e415628c1991899ea6afe4de15a96c
willoom <137672356+willoom@users.noreply.github.com>
2024-12-13 19:17:08 +0100

    Add files via upload
    Actividad para usar comparadores y generación controlada de números aleatorios.

---
+++ compara_numero_aleatorio.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# AUTOR: Willoom
+# FECHA: 13 de diciembre de 2024
+# DESCRIPCIÓN: 
+#		Este programa generará un número aleatorio entre uno y cien.
+#		Mostrará por pantalla qué número se ha generado.
+#		Si el número es inferior a 30 se dirá que es menor que 30.
+#		Si el número es mayor de 80 se indicará.
+#		En otro caso se indicará que el número aleatorio está entre 30 y 80, incluidos.
+
+
