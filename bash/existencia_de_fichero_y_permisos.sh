ef9380244bc0726ef2b91464722e5dac13210fa1
willoom <137672356+willoom@users.noreply.github.com>
2024-12-13 20:11:10 +0100

    Create existencia_de_fichero_y_permisos.sh
    Actividad para el refuerzo de if anidados y tratamiento de la información de archivos del sistema de ficheros ext4.

---
+++ bash/existencia_de_fichero_y_permisos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# AUTOR: Willoom
+# FECHA: 13 de diciembre de 2024
+# DESCRIPCIÓN: 
+#		Este programa solicitará al usuario una ruta a un posible fichero.
+#   En primer lugar detremina si el fichero existe en su condición de 
+#   fichero. Si es así imprime el mensaje:
+#   "El fichero X existe y tenemos pemisos de..."
+#   En otro caso se imprimirá por pantalla el mensaje:
+#   "El fichero X NO existe."
+
+#   Luego comprobaremos si tenemos permiso de lectura.
+#   Si es así mostraremos el mensaje "... lectura."
+
+#   Luego comprobaremos si tenemos permiso de escritura.
+#   Si es así mostraremos el mensaje "... escritura."
+
+#   Luego comprobaremos si tenemos permiso de ejecución.
+#   Si es así mostraremos el mensaje "... ejecución."
