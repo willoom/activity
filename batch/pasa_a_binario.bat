e0508545c024345eed16380c0437fd59b2771b7b
willoom <137672356+willoom@users.noreply.github.com>
2024-12-18 17:22:38 +0100

    Create pasa_a_binario.bat
    Problema para trabajar introducción de datos, traducción de numérico a cadena de caracteres, operación resto y saltos.

---
+++ batch/pasa_a_binario.bat
@@ -0,0 +1,20 @@
+@echo off
+
+rem Establecemos la página de códigos para que caracteres especiales 
+rem sean imprimibles
+chcp 65001
+
+rem Imprime por pantalla el mensaje 
+rem Introduzca el número a traducir a binario
+rem y almacénalo en una variable.
+
+rem Imoplementa el método de las divisiones sucesivas para ir generando 
+rem una cadena de caracteres con ceros y unos que almacenarás en la cvariable
+rem binario
+
+rem Es probable que tengáis que usar if, etiquetas y la orden goto.
+
+rem imprime por pantalla el mensaje
+rem El número decimal X es Y en binario.
+rem donde X es el nñumero decimal introducido por el usuario e
+rem Y la cadena de caracteres equivalente en binario a X.
