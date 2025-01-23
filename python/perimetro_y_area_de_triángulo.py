2d70b786edcf037078198e6f0636b907f9a5a0dd
willoom <137672356+willoom@users.noreply.github.com>
2025-01-23 12:11:09 +0100

    Create perimetro_y_area_de_triángulo.py
    Nuevo ejercicio de input y math.

---
+++ python/perimetro_y_area_de_triángulo.py
@@ -0,0 +1,7 @@
+"""
+AUTOR: Willoom
+FECHA: 23 de enero de 2025
+
+Este programa solicita por pantalla tres lados de un triámngulo y 
+muestra el perímetro y su área, con las unidades métricas del S.I.
+"""
