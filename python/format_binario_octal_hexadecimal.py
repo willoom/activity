353cad4740d5e70f64b4989f598ad4008480b796
willoom <137672356+willoom@users.noreply.github.com>
2025-01-23 10:36:08 +0100

    Update format_binario_octal_hexadecimal.py
    Corrección de errata y fecha de primera versión.

--- python/format_binario_octal_hexadecimal.py
+++ python/format_binario_octal_hexadecimal.py
@@ -1,10 +1,11 @@
 """
 AUTOR: Willoom
+FECHA: 23 de enero de 2025
 
 Escribe por pantalla la conversión a binario, octal y hexadecimal de 23, 55 y 1024
 usando el método format de la clase String.
 
 Éste método sólo recibirá un número por vez y ha de mostrar
 
-El número NNN en biinario es BBB, en octal es OOO y en hexadecimal es HHH."
+El número NNN en binario es BBB, en octal es OOO y en hexadecimal es HHH."
 """
