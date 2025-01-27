676f3261c9266aa702c3d3a6d5313001dbb4b3e7
willoom <137672356+willoom@users.noreply.github.com>
2025-01-27 09:34:15 +0100

    Create interes_compuesto.py

---
+++ python/interes_compuesto.py
@@ -0,0 +1,17 @@
+"""
+AUTOR: Willoom
+FECHA: 27 de enero de 2025
+
+Este programa solicita por pantalla: 
+* un capital inicial C en euros
+* una tasa de interes i en tanto por ciento y 
+* un número n de años
+
+Muestra cuál será el capital final aplicando el interés compuesto
+durante los n años.
+
+Propuesta a la actividad 40 página 80 del libro
+Introducción a la orgramación con Python 3, de la
+Universitat Jaume I
+ISBN: 978-84-697-1178-1
+"""
