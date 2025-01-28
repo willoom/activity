2fedd16b66cf92045a0a0c4b4fe1e92454e79b49
willoom <137672356+willoom@users.noreply.github.com>
2025-01-28 10:19:38 +0100

    Create no_puedes_entrar.py
    Ejercicio de control de acceso a discoteca.

---
+++ python/no_puedes_entrar.py
@@ -0,0 +1,18 @@
+"""
+AUTOR: Willoom
+FECHA: 27 de enero de 2025
+
+Deseamos hacer un programa que gestione la entrada a una discoteca.
+
+Al usuario se le solicita el año en que nació.
+El programa calacula su edad y en función de eso, imprime alguno de los siguientes mensajes.
+
+Si el usuario tiene menos de dieciocho años ha de decir, "Lo siento, no tienes la edad suficiente".
+
+Si el usuario, siendo mayor de edad tiene más de cincuenta, entonces deberá decir: "El bingo es en la sala de abajo".
+
+Si tiene más de 70 debería mostrar por pantalla que "¿No es la hora de tu medicación?".
+
+En otro caso "Bienvenido, puedes pasar".
+
+"""
