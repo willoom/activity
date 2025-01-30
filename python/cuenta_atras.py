536e19967f3e420280057729e2d02049cdd2dd6a
willoom <137672356+willoom@users.noreply.github.com>
2025-01-30 10:50:13 +0100

    Add files via upload
    Ejercicio con bucles y control de entrada.

---
+++ python/cuenta_atras.py
@@ -0,0 +1,15 @@
+"""
+AUTOR: Willoom
+FECHA: 30 de enero de 2025
+
+Deseamos hacer un programa que realice por pantalla una cuenta atrás.
+
+Pedirá al usuario un entero positivo, que hemos de comprobar.
+
+Si no lo fuera, le informaremos y volveremos a solicitar el entero positivo.
+
+Una vez tengamos el entero positivo, haremos una cuenta atrás desde el número introducido hasta el cero.
+
+Al final, se imprimirá el mensaje "¡DESPEGUE!"
+
+"""
