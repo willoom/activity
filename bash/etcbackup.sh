36a4f6b85f0180c37f8bd9ae548bcdbede19f836
willoom <137672356+willoom@users.noreply.github.com>
2025-01-31 19:17:31 +0100

    Add files via upload
    Activity about bash scripting, tar, date and systems.

---
+++ bash/etcbackup.sh
@@ -0,0 +1,18 @@
+: '
+Sxcript de backup de directorio de configuración del sistema /etc
+
+Crea un script llamado etcbackup.sh que en el directorio /root/backup guarde un fichero con nombre 
+YYYYMMDD_HHmm_etc.tar.bz2, que sea compilación comprimida con algoritmo Bzip2 del directorio de 
+configuración general del sistema /etc.
+
+Asume que lo ejecutará root.
+
+Si el directorio /root/backup no existiese, informará que de que se creará (y lo creará).
+
+Para hacer este script hay que extrar los datos necesarios de date.
+
+Recuerda usar la cabecera adecuada de los scripts de bash y darle los permisos oportunos de ejecución al 
+script.
+
+Extrema las medidas de seguridad a la hora de invocar comandos que no pertenezcan a bash.
+'
