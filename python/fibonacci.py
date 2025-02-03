713c09b702b24f088d9e553233d2e7fd5b9c2da8
willoom <137672356+willoom@users.noreply.github.com>
2025-02-03 10:27:49 +0100

    Create fibonacci.py
    Ejercicio de recursividad y funciones en python.

---
+++ python/fibonacci.py
@@ -0,0 +1,19 @@
+"""
+AUTOR: Willoom
+FECHA: 3 de febrero de 2025
+
+Crea una función llamada fibonacci.
+
+Los números de Fibonacci siguen la secuencia 
+1,1,2,3,5,8,13,21,34,55...
+
+Imprime por pantalla
+
+El término 4 de Fibonacci es 3.
+El término 7 de Fibonacci es XXX.
+El término 50 de Fibonacci es XXX.
+
+Donde XXX es el valor calculado por vuestra función.
+
+Usa recursividad.
+"""
