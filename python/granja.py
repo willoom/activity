23e395382533608e6fc14e0d03100c777a992139
willoom <137672356+willoom@users.noreply.github.com>
2025-04-28 09:32:40 +0200

    Create granja.py
    Ejercicio de subclases en python.

---
+++ python/granja.py
@@ -0,0 +1,41 @@
+"""
+AUTOR: Willoom
+FECHA: 28 de abril de 2025
+
+Este script necesita de las librerías cerdo.py, caballo.py, oveja.py, 
+perro.py, vaca.py y cualquier otra auxiliar que consideréis necesaria.
+
+Tendremos que declarar varios animales sabiendo que todo animal tiene 
+un nombre.
+
+No obstante cada uno habla de una manera.
+El cerdo dice oink, el caballo dice hiiii, la oveja dice beeee, el 
+perro dice guau y la vaca dice muuu.
+
+Tenéis que crear una lista con dos cerdos dándole nombres, 
+otra lista de cuatro ovejas, una lista de un perro y dos vacas.
+
+1º Tenéis que hacer las librerías de clases necesarias e importarlas.
+Deberéis organizarlas de forma que se favorezca el mantenimiento 
+del código. Obviemente tendréis que hacer uso de la herencia de clase
+y de la redefnición de métodos en las subclases.
+
+2º Tenéis que unir la lista de animales.
+
+3º print("Egidio, el granjero de Ham, saluda a sus animales.")
+
+4º Debe mostrarse por pantalla algo similar a:
+El cerdo Babe dice: oink!
+El cerdo Tate dice: oink!
+La oveja Dolly dice: beee!
+La oveja Polly dice: beee!
+La oveja Colly dice: beee!
+La oveja Zolly dice: beee!
+El perro Rex dice: guau!
+La vaca Lola dice: muuu!
+La vaca Pepa dice: muu!
+
+En un mismo bucle debéis iterar sobre los objetos de la clase 
+Animal y dar una salida por pantalla similar a la anterior.
+"""
+print("Egidio, el granjero de Ham, saluda a sus animales.")
