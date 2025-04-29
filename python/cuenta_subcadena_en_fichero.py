4bc277e65f1c34226cf0c3b820079234108c64c5
willoom <137672356+willoom@users.noreply.github.com>
2025-04-29 10:59:16 +0200

    Create cuenta_subcadena_en_fichero.py
    Ejercicio de lectura de ficheros y subcadenas.

---
+++ python/cuenta_subcadena_en_fichero.py
@@ -0,0 +1,14 @@
+"""
+AUTOR: Willoom
+FECHA: 29 de abril de 2025
+
+Solicita al usuario un path a un fichero que desee analizar.
+
+Solicita al usuario una subcadena que buscar.
+
+Cuenta el número de apariciones de la subcadena en el fichero e informa 
+con el mensaje:
+
+La subcadena "SUBCADENA" aparece X veces en el fichero FICHERO.
+"""
+
