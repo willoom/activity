50c4584f39fd1bd4e8892abbb836755bc4525488
willoom <francisco.yuste@gmail.com>
2025-10-08 15:50:19 +0200

    Actividad de gestiónde fichero línea a línea.

---
+++ bash/comenta_en_bash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Autor: Francisco Yuste García
+# Fecha: 8 de octubre de 2025
+# Fichero : comenta_en_bash.sh
+
+# Descripción del ejercicio:
+# Escribe un script que recibe como parámetro un texto
+# y que lo comenta precedido con "# "
+# Haz de emplear read, while y la redirección a entrada estándar.
+
+# El fichero a comentar se recibe como un primer parámetro.
