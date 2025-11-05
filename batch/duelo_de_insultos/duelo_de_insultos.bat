c5e372aa827248c4f165e1fdc153bd261e78c7c3
willoom <francisco.yuste@gmail.com>
2025-11-05 16:58:17 +0100

    ejercicio para reforzar el uso de CHOICE y ERRORLEVEL

---
+++ batch/duelo_de_insultos/duelo_de_insultos.bat
@@ -0,0 +1,25 @@
+@echo off
+
+REM AUTOR: Willoom.
+
+REM FECHA: 5 de noviembre de 2025.
+
+REM RESUMEN: 
+REM    Muestra un insulto de Monkey Island 1 y cinco opciones, entre las que se encuentra la correcta.
+REM    Si el usuario elige la correcta se muestra el mensaje 
+REM      ¡Te vencí mequetrefe!
+REM    Si el usuario elige una opción incorrecta, muestra el mensaje
+REM      Me has vencido. Me uno a tu tripulación. 
+REM    Con CHOICE el script asegurará sólo opciones válidas.
+REM    Con ERRORLEVEL tratará la opción elegida por el usuario.
+
+REM SABER MÁS: 
+REM    La lista de insultos y respuestas está en
+REM    https://gamelosofy.com/los-insultos-de-el-secreto-de-monkey-island-1/
+
+
+
+REM Establecemos la página de códigos para representar los caracteres especiales de nuestro idioma.
+
+chcp 65001
+
