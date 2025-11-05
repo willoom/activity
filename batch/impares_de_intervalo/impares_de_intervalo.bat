0a4a7042d89f3b04fe53b74f91e5fdab92f8603f
willoom <francisco.yuste@gmail.com>
2025-11-05 16:12:30 +0100

    ejercicio de operaciones aritméticas, lectura de variables y for

---
+++ batch/impares_de_intervalo/impares_de_intervalo.bat
@@ -0,0 +1,16 @@
+@echo off
+
+REM AUTOR: Willoom.
+
+REM FECHA: 5 de noviembre de 2025.
+
+REM RESUMEN: 
+REM    Este script solicita dos números.
+REM    Comprueba que son enteros positivos sin signo.
+REM    Comprueba que el primero es menos que el segundo.
+REM    Imprime los impares entre los dos números indicados.
+
+REM Establecemos la página de códigos para representar los caracteres especiales de nuestro idioma.
+
+chcp 65001
+
