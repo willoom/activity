70a7638f91431bfc9a1409789bcd8939ccfe5bef
willoom <francisco.yuste@gmail.com>
2025-11-06 10:32:15 +0100

    Corrección de erratas en comentarios y cadenas de caracteres.

--- batch/duelo_de_insultos/duelo_de_insultos.bat
+++ batch/duelo_de_insultos/duelo_de_insultos.bat
@@ -5,9 +5,10 @@ REM AUTOR: Willoom.
 REM FECHA: 5 de noviembre de 2025.
 
 REM RESUMEN: 
-REM    Muestra un insulto de Monkey Island 1 y cinco opciones, entre las que se encuentra la correcta.
+REM    Muestra un insulto de Monkey Island 1 y cinco opciones, entre las que 
+REM    se encuentra la correcta.
 REM    Si el usuario elige la correcta se muestra el mensaje 
-REM      ¡Te vencí mequetrefe!
+REM      ¡Te vencí, mequetrefe!
 REM    Si el usuario elige una opción incorrecta, muestra el mensaje
 REM      Me has vencido. Me uno a tu tripulación. 
 REM    Con CHOICE el script asegurará sólo opciones válidas.
@@ -16,8 +17,6 @@ REM    Con ERRORLEVEL tratará la opción elegida por el usuario.
 REM SABER MÁS: 
 REM    La lista de insultos y respuestas está en
 REM    https://gamelosofy.com/los-insultos-de-el-secreto-de-monkey-island-1/
-
-
 
 REM Establecemos la página de códigos para representar los caracteres especiales de nuestro idioma.
 
