d6dd94ab2b76d145ec725fb8f8301d41b6a14220
willoom <francisco.yuste@gmail.com>
2026-01-20 13:06:01 +0100

    Ejercicio de tratamiento de parámetros.

---
+++ c/argv/viaje_con_nosotros.c
@@ -0,0 +1,49 @@
+/*
+AUTOR: Francisco Yuste Garccía
+FECHA: 20 de enero de 2026
+
+Escribe el programa viaje_con_nosotros.exe
+
+Cuando lo invoquemos con la forma:
+
+> viaje_con_nosotros.exe Fortúnez Lucas 44 Maldivas primavera Tatiana Ribera Olmo
+
+El script debe mostrar por pantalla:
+
+Estimad@ Lucas Fortúnez,
+
+Gracias por confiar en Jilguero Viajes en su 44 aniversario.
+
+Maldivas en primavera es un paraje encantador.
+
+Confiamos en que le guste.
+
+Atentamente,
+
+-T.R.O.
+
+NIVELES DE ACABADO
+
+Tenéis que comentar cuanto hagais pormenorizadamente.
+
+____ ARRANQUE ____
+Definís las variables 
+cliente_apellido, cliente_nombre, 
+cliente_edad, destino, 
+estacion, 
+comercial_nombre, comercial_apellido1, comercial_apellido2
+y les asignáis 
+
+____ BASICO ____
+Generáis la carta de confirmación de viaje hasta confiamos que le guste.
+
+____ AVANZADO ____
+A partir de los datos recibidos, creáis el pie de la carta.
+
+____ EXTRAORDINARIO ___
+Colorea en verde y negrita los datos del viajero y azul y negrita las iniciales del comercial.
+
+
+*/
+
+
